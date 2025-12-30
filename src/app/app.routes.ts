159e2fdf566895a54bf15cdc9a126a2ca015f422
Nathan-Sasa <jonathanbikuta05@gmail.com>
2025-12-30 16:27:50 +0100

    gereration d'une page <profile>

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,19 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-  {
-    path: 'home',
-    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
-  },
-  {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full',
-  },
+	{
+		path: 'home',
+		loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
+	},
+	{
+		path: 'profile',
+		loadComponent: ()=> import('./home/profile/profile.page').then(m => m.ProfilePage)
+	},
+	
+
+	{
+		path: '',
+		redirectTo: 'home',
+		pathMatch: 'full',
+	},
 ];
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,11 +1,20 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { RouterModule } from '@angular/router';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton } from '@ionic/angular/standalone';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+	selector: 'app-home',
+	templateUrl: 'home.page.html',
+	styleUrls: ['home.page.scss'],
+	imports: [
+		IonHeader, 
+		IonToolbar, 
+		IonTitle, 
+		IonContent, 
+		IonButton,
+		RouterModule
+	],
 })
 export class HomePage {
   constructor() {}
